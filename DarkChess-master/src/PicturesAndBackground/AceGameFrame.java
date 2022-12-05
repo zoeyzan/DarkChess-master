aacd4a0710705116c3f8ed373d5bdeee6f3f56f0
LinkApollo <119751584+LinkApollo@users.noreply.github.com>
2022-12-05 15:59:09 +0800

    fuckyeah

--- DarkChess-master/src/PicturesAndBackground/AceGameFrame.java
+++ DarkChess-master/src/PicturesAndBackground/AceGameFrame.java
@@ -13,7 +13,7 @@ public class AceGameFrame extends JFrame{
         setResizable(false);
         add(new GamePanel());
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); //设置程序关闭按键，如果点击右上方的叉就游戏全部关闭了
-        setLayout(null);
+
         setVisible(true);
         setTitle("Ace Attorney: Dark Chess Apollo vs Klavier");
         //记得加按钮，鼠标判定，命令。
