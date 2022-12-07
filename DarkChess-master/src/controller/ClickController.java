cb6935826b6ed1e2e91933f7f79e064328b9a166
LinkApollo <119751584+LinkApollo@users.noreply.github.com>
2022-12-07 15:47:39 +0800

    2

--- DarkChess-master/src/controller/ClickController.java
+++ DarkChess-master/src/controller/ClickController.java
@@ -1,7 +1,7 @@
 package controller;
 
 
-import PicturesAndBackground.AceGameFrame;
+//import PicturesAndBackground.AceGameFrame;
 import chessComponent.CannonChessComponent;
 import chessComponent.ChessComponent;
 import chessComponent.SquareComponent;
@@ -76,7 +76,7 @@ public class ClickController {
 
         if (!squareComponent.isReversal()) {
             //没翻开且非空棋子不能走
-        if(first instanceof CannonChessComponent&&!(squareComponent instanceof EmptySlotComponent))
+            if(first instanceof CannonChessComponent&&!(squareComponent instanceof EmptySlotComponent))
                 return first.canMoveTo(chessboard.getChessComponents(), squareComponent.getChessboardPoint());
             if (!(squareComponent instanceof EmptySlotComponent)) {
                 return false;
@@ -87,14 +87,15 @@ public class ClickController {
     }
 
     //  老师写的，需要测试逻辑的时候取消注释，把我的加上注释
-    /*
+
     public void swapPlayer() {
         chessboard.setCurrentColor(chessboard.getCurrentColor() == ChessColor.BLACK ? ChessColor.RED : ChessColor.BLACK);
         ChessGameFrame.getStatusLabel().setText(String.format("%s's TURN", chessboard.getCurrentColor().getName()));
     }
 
 
-     */
+
+    /*
 
     public void swapPlayer() {
         chessboard.setCurrentColor(chessboard.getCurrentColor() == ChessColor.BLACK ? ChessColor.RED : ChessColor.BLACK);
@@ -102,4 +103,6 @@ public class ClickController {
     }
 
 
+
+     */
 }
